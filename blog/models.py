a33a9d7cdf4b4dce1bff348bb112a08e9f22179e
Jordano98 <mohammad.dehghan218@gmail.com>
2022-06-30 23:46:59 +0430

    complete the category part
    work with manytomany feild in this part and make connection between two tables(Post and Category)

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,25 @@
 from distutils.command.upload import upload
+from sre_parse import Verbose
+from tabnanny import verbose
 from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model) :
+    name=models.CharField(max_length=225)
+
+    def __str__(self) :
+        return self.name
+    class Meta:
+        verbose_name_plural='categories'
 class Post(models.Model):
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True) 
     title = models.CharField(max_length=255)
     content = models.TextField()
     # tags = 
-    # category = 
+    category = models.ManyToManyField(Category) 
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
