cb1b1f2994208ab161ba2954f5bc384dc8095f03
Jordano98 <mohammad.dehghan218@gmail.com>
2022-07-03 13:52:01 +0430

    make some examination about prev and next opportunity

--- blog/views.py
+++ blog/views.py
@@ -5,20 +5,22 @@ from blog.models import Post
 # Create your views here.
 
 def blog_view(request):
-    posts=Post.objects.filter(published_date__lte =Now())
-    context={'posts':posts}
+    posts=Post.objects.filter(published_date__lte =Now(),status=1)
+    context={'posts':posts }
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
-    post=get_object_or_404(Post, pk=pid,status=1)
+    posts=Post.objects.filter(published_date__lte =Now(),status=1)
+    len_posts=len(posts)
+    post=get_object_or_404(Post, pk=pid,published_date__lte =Now(),status=1)
     def counter ():
         post.counted_views +=1
         post.save()
     counter()
-    context={'post':post}
+    context={'post':post ,'len_posts':len_posts}
     return render(request,'blog/blog-single.html',context)
 
-def blog_test (request,pid) :
+'''def blog_test (request,pid) :
     post=get_object_or_404(Post, pk=pid)
     context={'post':post}
-    return render(request,'test.html',context)
+    return render(request,'test.html',context)'''
