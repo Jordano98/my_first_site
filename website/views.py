ec193b840ac1881a1d253dc435cbc11da894bf94
Jordano98 <mohammad.dehghan218@gmail.com>
2022-07-14 21:37:42 +0430

    work with forms in pages without model forms(simple way))

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
+from tkinter import E
 from django.shortcuts import render
+from website.models import Contact
 
 def index_view(request) :
     return render(request,'website/index.html')
@@ -11,3 +13,17 @@ def contact_view(request):
 
 def elements_view(request):
     return render(request,"website/elements.html")
+
+def test_view(request):
+    if request.method=='POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        massage=request.POST.get('massage')
+        c= Contact()
+        c.name=name
+        c.email=email
+        c.subject=subject
+        c.message=massage
+        c.save()
+    return render(request,"test.html")
