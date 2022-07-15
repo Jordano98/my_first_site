9d19a4152df7f872f91e4c7e89cdf5738f8855f9
Jordano98 <mohammad.dehghan218@gmail.com>
2022-07-15 13:10:21 +0430

    work with forms.form in django as  a primarytest

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from tkinter import E
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from website.models import Contact
+from website.forms import NameForm
 
 def index_view(request) :
     return render(request,'website/index.html')
@@ -16,14 +16,25 @@ def elements_view(request):
 
 def test_view(request):
     if request.method=='POST':
-        name=request.POST.get('name')
-        email=request.POST.get('email')
-        subject=request.POST.get('subject')
-        massage=request.POST.get('massage')
-        c= Contact()
-        c.name=name
-        c.email=email
-        c.subject=subject
-        c.message=massage
-        c.save()
-    return render(request,"test.html")
+        form=NameForm(request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['name']
+            email=form.cleaned_data['email']
+            subject=form.cleaned_data['subject']
+            massage=form.cleaned_data['massage']
+            print(name,email,subject,massage)
+            return HttpResponse('done')
+            
+        else:
+            return HttpResponse('not valid')
+        # email=request.POST.get('email')
+        # subject=request.POST.get('subject')
+        # massage=request.POST.get('massage')
+        # c= Contact()
+        # c.name=name
+        # c.email=email
+        # c.subject=subject
+        # c.message=massage
+        # c.save()
+    form=NameForm()
+    return render(request,"test.html",{'form':form})
