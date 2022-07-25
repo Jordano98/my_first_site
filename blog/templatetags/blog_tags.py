202045210b299ff83b9714cfe5975ad8e650d2cc
Jordano98 <mohammad.dehghan218@gmail.com>
2022-07-25 15:41:43 +0430

    completion of comment parts of template

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post,Category
+from blog.models import Post,Category,Comment
 register= template.Library()
 
 @register.simple_tag(name='totalposts')
@@ -11,6 +11,11 @@ def function() :
 def function() :
     posts=Post.objects.filter(status=1)
     return posts
+
+@register.simple_tag(name='comment-count')
+def function(pid) :
+    return Comment.objects.filter(post=pid).count()
+
 
 @register.filter
 def snippet(value,arg=20):
