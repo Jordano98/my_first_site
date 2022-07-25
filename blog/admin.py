f4582c0cf43bcde0de9d36c7b6e314885d66840f
Jordano98 <mohammad.dehghan218@gmail.com>
2022-07-25 14:29:32 +0430

    making a comment model for commenting process
    make an admin panel controller
    make a foreign key for connection between posts and comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post,Category
+from blog.models import Post,Category,Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -15,3 +15,11 @@ admin.site.register(Post,PostAdmin)
 
 
 admin.site.register(Category)
+
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy='created_date'
+    empty_value_display='-empty-'
+    list_display = ('name' ,'post','email','approach','created_date')
+    list_filter = ('post','approach')
+    search_fields = ['name','post']
+admin.site.register(Comment,CommentAdmin)
