bab4046a2060375437ca0a401725071149dcbfa9
Jordano98 <mohammad.dehghan218@gmail.com>
2022-07-26 15:00:56 +0430

    some extra edition after branch merging

--- blog/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from blog.models import Comment
-
-class commentform(forms.ModelForm):
-
-    class Meta :
-        model=Comment
-        fields= ['name','email','subject','massage']
