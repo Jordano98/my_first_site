d66ed3cc846791bced13f03beab02a05774d051b
Jordano98 <mohammad.dehghan218@gmail.com>
2022-07-26 15:26:43 +0430

    making rss feed

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from blog.views import *
-
+from blog.feeds import LatestEntriesFeed
 app_name="blog"
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     path('author/<str:author_username>',blog_view,name="author"),
     path('test',blog_test,name="test"),
     path('search/',blog_search,name="search"),
+     path('rss/feed/', LatestEntriesFeed()),
 ]
