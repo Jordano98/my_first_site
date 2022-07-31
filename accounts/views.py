2b8996b4ac351d74063ce9b51a019ddd7690846d
Jordano98 <mohammad.dehghan218@gmail.com>
2022-08-01 00:42:36 +0430

    completion of accounts part
    work on user logging in login.html page and I used DJANGO authentication docs

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
 
 def login_view(request):
-    return (request,'accounts/login.html')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+    return render (request,'accounts/login.html')
 
 #def logout_view(request):
     #return (request,'accounts/login.html')
 
 def signup_view(request):
-    return (request,'accounts/signup.html')
+    return render (request,'accounts/signup.html')
 
 # Create your views here.
