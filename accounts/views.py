3879ba0ec5c054451f80b11249f38d9d68e76934
Jordano98 <mohammad.dehghan218@gmail.com>
2022-08-01 12:15:05 +0430

    completion of logout function
    we used built-in @login_required decorator

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 def login_view(request):
     if not request.user.is_authenticated:
         if request.method == 'POST':
@@ -17,8 +18,12 @@ def login_view(request):
         return render (request,'accounts/login.html',context)
     else:
         return redirect('/')
-#def logout_view(request):
-    #return (request,'accounts/login.html')
+
+@login_required
+def logout_view(request):
+    #if request.user.is_authenticated:
+    logout(request)
+    return redirect('/')
 
 def signup_view(request):
     return render (request,'accounts/signup.html')
